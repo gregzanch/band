613f13573a5fe4733dff6ab5f7858b91a10e9787
gregzanch <zanchelli.greg@gmail.com>
2022-05-03 18:18:02 -0400

    try a different approach

--- src/helpers/api/cors.ts
+++ src/helpers/api/cors.ts
@@ -4,9 +4,8 @@ type ApiFn = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
 
 export const allowCors = (fn: ApiFn) => async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
   res.setHeader(
     "Access-Control-Allow-Headers",
